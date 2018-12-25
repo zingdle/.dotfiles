aec81fe056ad80c63461ab9fdb18b9df3ea4dfcd
Jiading Guo <zingdle@gmail.com>
2018-12-25 19:09:25 +0800

    save

--- zsh/config.sh
+++ zsh/config.sh
@@ -4,7 +4,7 @@ cp -rf $DIR/.zshrc $HOME
 
 # oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
 cp -rf $DIR/robbyrussell-fix.zsh-theme $ZSH/themes
