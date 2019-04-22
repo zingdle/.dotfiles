d69c69e46ebff2c0061175dc11b182163a7384b6
Jiading Guo <zingdle@gmail.com>
2019-04-22 10:06:30 +0800

    save

--- zsh/config.sh
+++ zsh/config.sh
@@ -8,7 +8,7 @@ if [ ! -d ~/.oh-my-zsh ]; then
   sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
-cp -rf $DIR/robbyrussell-fix.zsh-theme $ZSH/themes
+cp -rf $DIR/robbyrussell-fix.zsh-theme $ZSH/custom/themes
 
 if [ ! -d $ZSH/custom/plugins/zsh-autosuggestions ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
